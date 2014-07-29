f5bc177b455acd48adb8c3d7704ba3b94129222b
Benjamin Brodeur Mathieu <benjaminbrodeur@gmail.com>
2014-07-28 20:19:24 -0400

    first init

---
+++ Assets/Scipts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private spinButton spinButtton_;
+	private exitButton exitButton_;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spinButtton_ = (spinButton)GameObject.FindGameObjectWithTag("spinButton").GetComponent(typeof(spinButton));
+		exitButton_ = (exitButton)GameObject.FindGameObjectWithTag("exitButton").GetComponent(typeof(exitButton));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int i = 0;
+		while (i < Input.touchCount) 
+		{
+			if (Input.GetTouch(i).phase == TouchPhase.Ended && spinButtton_.IsInButton(Input.GetTouch(i).position))
+			{
+				spinButtton_.isClicked();
+            }
+			if (Input.GetTouch(i).phase == TouchPhase.Ended && exitButton_.IsInButton(Input.GetTouch(i).position))
+			{
+				exitButton_.isClicked();
+            }
+            
+            ++i;
+        }
+    }
+}
---
+++ Assets/Scipts/Spinning.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinning : MonoBehaviour 
+{
+	public float initialSpeed;
+	public bool enableSpin;
+	private float timer;
+	private Vector3 startingAngle;
+
+	// MYOWNLERP
+	private float elapsedTime;
+	private float initTime;
+
+
+	void Start()
+	{
+		startingAngle = transform.eulerAngles;
+	}
+
+
+	// Update is called once per frame before any physics calculations
+	void FixedUpdate () 
+	{
+		if (enableSpin) 
+		{
+			Spin();
+		}
+	}
+
+	public void Spin()
+	{
+		Vector3 origin = new Vector3 (0,0,0);
+		Vector3 ZAxis = new Vector3 (0,0,1);
+		transform.RotateAround (origin, ZAxis,Time.deltaTime * -calculateSpeed());
+	}
+
+	public void startSpin()
+	{
+		timer = 10f;
+		initTime = Time.time;
+		elapsedTime = 0f;
+		transform.Rotate(startingAngle);
+
+		enableSpin = true;
+	}	
+
+	private float calculateSpeed()
+	{
+		elapsedTime = Time.time - initTime;
+		float result = -((initialSpeed)/timer)*elapsedTime + initialSpeed;
+
+		if(result <= 0)
+		{
+			enableSpin = false;
+		}
+
+		return result;
+	}
+}
